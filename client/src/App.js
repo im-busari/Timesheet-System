2655b0277b61617de78d46a465b7e0f70a45f6b3
Kristiyan Nedyalkov <kristiyan.nedyalkov@mentormate.com>
2020-09-30 13:59:43 +0300

    Add FormInput component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import React from "react";
 
 function App() {
   return <div className="App"></div>;
