7b17a1fc0f0d1604003bf0db096c6eb3c182e556
Angel Bozhkov <angel.bozhkov@mentormate.com>
2020-09-30 14:51:47 +0300

    Navigation and generics Logo, Title

---
+++ client/src/components/Navigation/NavigationStyledComponents.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { Nav } from "react-bootstrap";
+
+export const StyledNav = styled(Nav)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--blue);
+  padding: 1rem;
+`;
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const StyledNavItem = styled(Nav.Item)`
+  list-style-type: none;
+  transition: transform 0.5s;
+  :hover {
+    transform: scale(1.125);
+  }
+`;
+export const StyledNavLink = styled(Nav.Link)`
+  color: var(--white);
+  :hover {
+    color: var(--white);
+  }
+`;
