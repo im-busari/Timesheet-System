a5d1c7f51117cf9e0ee34797a9cf8c277dbd45d6
Kristiyan Nedyalkov <kristiyan.nedyalkov@mentormate.com>
2020-09-30 13:51:39 +0300

    Remove unneeded imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const renderApp = () => {
-	ReactDOM.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		document.getElementById("root")
-	);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 };
 
 // If you want your app to work offline and load faster, you can change
@@ -19,7 +18,7 @@ const renderApp = () => {
 serviceWorker.unregister();
 
 if (module.hot) {
-	module.hot.accept("./App", renderApp);
+  module.hot.accept("./App", renderApp);
 }
 
 renderApp();
