b221f899a2d1ec5e27f35af07454bf9d1f9d859a
Daniel Zhelyazkov <daniel.zhelyazkov@mentormate.com>
2020-09-30 17:20:10 +0300

    Deleting timesheet implemented.

--- server/controllers/TimesheetController.js
+++ server/controllers/TimesheetController.js
@@ -1,12 +1,60 @@
+const {Timesheet} = require('../models');
 
 class TimesheetController {
 
-    createTimesheet = async (req, res) => {
+    async createTimesheet(req, res) {
         try {
+            // Finds if the user already has a timesheet for this week
+            const allTimesheet = await Timesheet.findOne({
+                where: {
+                    startDate: req.body.startDate,
+                    userId: req.body.userId,
+                }
+            });
 
-            res.status(200).send('example Controller');
+            let timesheet;
+
+            if (!allTimesheet) {
+                timesheet = await Timesheet.create({
+                    status: 'Open',
+                    startDate: req.body.startDate,
+                    userId: req.body.userId,
+                });
+            }
+
+            if (timesheet) {
+                res.status(201).send(timesheet.toJSON());
+            } else {
+                res.status(409).send({error: 'Already created!'});
+            }
+
         } catch (err) {
-            res.status(500).send('Something is wrong with our server.');
+            res.status(403).json(err);
+        }
+    }
+
+    async deleteTimesheet(req, res) {
+        try {
+            // Finds one by user id and start date.
+            const timesheet = await Timesheet.findOne({
+                where: {
+                    startDate: req.body.startDate,
+                    userId: req.body.userId
+                }
+            });
+
+            if (timesheet) {
+                await Timesheet.destroy({
+                    where: {
+                        id: timesheet.id
+                    }
+                });
+                res.status(201).send({success: 'Delete successfully!'});
+            } else {
+                res.status(409).json({error: 'Timesheet doesn\'t exist'})
+            }
+        } catch (err) {
+            res.status(403).json(err);
         }
     }
 }
--- server/routes/timesheet.js
+++ server/routes/timesheet.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const TimesheetController = require('../controllers/TimesheetController');
 
 router.post('/', TimesheetController.createTimesheet);
+router.delete('/', TimesheetController.deleteTimesheet);
 
 module.exports = router;
