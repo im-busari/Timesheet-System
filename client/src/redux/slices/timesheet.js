191e0cefc52293ff69a34c1e2a4f531f35f41163
Kristiyan Nedyalkov <kristiyan.nedyalkov@mentormate.com>
2020-10-02 14:01:25 +0300

    Complete timesheet reducer

--- client/src/redux/slices/timesheet.js
+++ client/src/redux/slices/timesheet.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { timesheet } from "../../api";
 
 const initialState = {
-  timesheets: [],
+  timesheets: {},
   getError: null,
   createError: null,
 };
@@ -12,16 +12,44 @@ const { reducer, actions } = createSlice({
   initialState,
   reducers: {
     fetch: (state, action) => {
-      return { ...state, timesheets: action.payload };
+      const timesheets = action.payload;
+
+      for (const timesheet of timesheets) {
+        const timesheetId = timesheet.data.id;
+
+        if (!state.timesheets[timesheetId]) {
+          state.timesheets[timesheetId] = {};
+        }
+
+        state.timesheets[timesheetId] = timesheet;
+      }
+
+      state.getError = null;
     },
     fetchError: (state, action) => {
       return { ...state, getError: action.payload };
     },
     create: (state, action) => {
-      state.timesheets.push(action.payload);
+      const timesheetId = action.payload.data.id;
+
+      if (!state.timesheets[timesheetId]) {
+        state.timesheets[timesheetId] = action.payload;
+      }
+
+      state.createError = null;
     },
     createError: (state, action) => {
       return { ...state, createError: action.payload };
+    },
+    delete: (state, action) => {
+      const timesheetId = action.payload;
+
+      if (state.timesheets[timesheetId]) {
+        delete state.timesheets[timesheetId];
+      }
+    },
+    deleteError: (state, action) => {
+      return { ...state, deleteError: action.payload };
     },
   },
 });
@@ -31,8 +59,8 @@ export { reducer as timesheetReducer, actions as timesheetActions };
 export const getTimesheetsForUser = () => {
   return async (dispatch) => {
     try {
-      const timesheets = await timesheet.get.allTimesheetsForUser();
-      console.log(timesheets);
+      const timesheetsRaw = await timesheet.get.allTimesheetsForUser();
+      const timesheets = timesheetsRaw.map((ts) => ts.timesheet);
       dispatch(actions.fetch(timesheets));
     } catch (error) {
       dispatch(actions.fetchError(error.message));
@@ -43,10 +71,24 @@ export const getTimesheetsForUser = () => {
 export const createTimesheet = ({ startDate }) => {
   return async (dispatch) => {
     try {
-      const newTimesheet = await timesheet.post.create({ startDate });
+      const newTimesheetRaw = await timesheet.post.create({ startDate });
+      const newTimesheet = { data: newTimesheetRaw, entries: [] };
+
       dispatch(actions.create(newTimesheet));
+      console.log("new", newTimesheet);
     } catch (error) {
       dispatch(actions.createError(error.message));
+    }
+  };
+};
+
+export const deleteTimesheet = ({ id }) => {
+  return async (dispatch) => {
+    try {
+      await timesheet.del.delete({ timesheetId: id });
+      dispatch(actions.delete(id));
+    } catch (error) {
+      dispatch(actions.deleteError(error.message));
     }
   };
 };
