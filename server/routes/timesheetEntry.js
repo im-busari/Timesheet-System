e4f8a8e3da530eb8fd14c1cedbfbd1a16274ce7a
Immanuella Busari <immanuela@mentormate.com>
2020-10-02 08:52:04 +0100

    getTimesheetById response changed

--- server/routes/timesheetEntry.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const TimesheetEntryController = require('../controllers/TimesheetEntryController');
-
-router.get('/days/:timesheetEntryId', TimesheetEntryController.getEntryDays);
-
-module.exports = router;
