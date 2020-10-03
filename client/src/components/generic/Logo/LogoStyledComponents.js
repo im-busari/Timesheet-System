c2bdc7baacac51b48b11c197ec4b763fab651b08
Angel Bozhkov <angel.bozhkov@mentormate.com>
2020-10-03 15:17:01 +0300

    navbar style

--- client/src/components/generic/Logo/LogoStyledComponents.js
+++ client/src/components/generic/Logo/LogoStyledComponents.js
@@ -5,12 +5,14 @@ export const MainContainer = styled.div`
   flex-direction: column;
   color: var(--white);
   cursor: pointer;
+  margin-left: 2rem;
   margin-right: 1rem;
 `;
 export const LineOne = styled.div`
   font-weight: 600;
-  font-size: 1.125rem;
+  font-size: 1.5rem;
 `;
 export const LineTwo = styled.div`
+  font-size: 1.25rem;
   margin-left: 2rem;
 `;
