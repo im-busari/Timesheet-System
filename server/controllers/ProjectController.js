bff018411a95f5e47a043c37b067bc4ab1e21bc5
Daniel Zhelyazkov <daniel.zhelyazkov@mentormate.com>
2020-10-04 09:32:52 +0300

    Docs added for all functions in project controller.

--- server/controllers/ProjectController.js
+++ server/controllers/ProjectController.js
@@ -1,43 +1,57 @@
-const { Project, Task } = require('../models');
+const {Project, Task} = require('../models');
 
 class ProjectController {
-  async getAllProjects(req, res) {
-    try {
-      const projects = await Project.findAll({
-        include: [
-          {
-            model: Task,
-            as: 'tasks',
-            required: false,
-            attributes: ['id', 'name'],
-            through: { attributes: [] },
-          },
-        ],
-      });
-      res.status(200).send({ projects });
-    } catch (err) {
-      res.status(500).send({ error: err });
+    /**
+     * GET /projects
+     *
+     * Gets all projects form the database.
+     *
+     * Returns an object containing all the projects data.
+     */
+    async getAllProjects(req, res) {
+        try {
+            const projects = await Project.findAll({
+                include: [
+                    {
+                        model: Task,
+                        as: 'tasks',
+                        required: false,
+                        attributes: ['id', 'name'],
+                        through: {attributes: []},
+                    },
+                ],
+            });
+            res.status(200).send({projects});
+        } catch (err) {
+            res.status(500).send({error: err});
+        }
     }
-  }
 
-  async getProjectById(req, res) {
-    try {
-      const projects = await Project.findByPk(req.params.projectId, {
-        include: [
-          {
-            model: Task,
-            as: 'tasks',
-            required: false,
-            attributes: ['id', 'name'],
-            through: { attributes: [] },
-          },
-        ],
-      });
-      res.status(200).send({ projects });
-    } catch (err) {
-      res.status(500).send({ error: err });
+    /**
+     * GET /projects/:projectId
+     *
+     * Gets a project form the database.
+     *
+     * Returns an object containing the project data.
+     */
+    async getProjectById(req, res) {
+        try {
+            const projects = await Project.findByPk(req.params.projectId, {
+                include: [
+                    {
+                        model: Task,
+                        as: 'tasks',
+                        required: false,
+                        attributes: ['id', 'name'],
+                        through: {attributes: []},
+                    },
+                ],
+            });
+            res.status(200).send({projects});
+        } catch (err) {
+            res.status(500).send({error: err});
+        }
     }
-  }
 }
 
 module.exports = new ProjectController();
